4cf1df2e2c8b20fdab8708ecbc806c065538b282
Tanmay <136565870+Tanmay9296@users.noreply.github.com>
2025-01-16 14:33:31 +0530

    Add files via upload

---
+++ Ai/app/components/space-background.tsx
@@ -0,0 +1,17 @@
+export function SpaceBackground() {
+  return (
+    <>
+      <div className="stars">
+        <div className="small"></div>
+        <div className="medium"></div>
+        <div className="big"></div>
+      </div>
+      <div className="planets">
+        <div className="planet planet-1"></div>
+        <div className="planet planet-2"></div>
+        <div className="planet planet-3"></div>
+      </div>
+    </>
+  )
+}
+
